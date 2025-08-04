ee459e6146f9f1c4806e8156ffba80af0281c06c
surjeetkumar8006 <surjeet6@amityonline.com>
2025-08-05 05:22:46 +0530

    add file

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,17 @@
-const mongoose=require('mongoose')
-exports.connectToDB = async () => {
-    mongoose.connect(process.env.Mongo_URI).then((connection)=>{
-        console.log("DB connected : "+ connection.connection.host);
-    }).catch(err=>{
-        console.log(err);
-    })
+require("dotenv").config({ path: "./config/config.env" }); // load env here too if needed
+
+const mongoose = require("mongoose");
+
+const connectToDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected!");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+  }
 };
+
+module.exports = { connectToDB };
